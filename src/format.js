4d961e962515ee0df0ac8b3a8b352705ac864a9e
Jonas Faber <dev.jonas.faber@gmail.com>
2017-09-29 10:13:06 +0200

    Add sparklines

--- src/format.js
+++ src/format.js
@@ -2,21 +2,20 @@ import { padStart } from 'lodash'
 
 export const board = state =>
   state.get('board').map(r => r.map(t => (t ? padStart(t, 4) : '    ')).join(' ')).join('\n')
+export const toData = e =>
+  [...e].map(e => ({
+    action: e.action,
+    board: board(e.nextGameState),
+    score: e.nextGameState.get('score'),
+    reward: e.reward,
+  }))
 
-export const episode = e =>
-  [...e].map(e => {
-    const data = {
-      action: e.action,
-      board: board(e.nextGameState),
-      score: e.nextGameState.get('score'),
-      reward: e.reward,
-    }
-    const log = `___________________
-Action: ${data.action}
-${data.board}
-
-Score: ${data.score}
-Reward: ${data.reward}`
-
-    console.log(log)
+export default episodes =>
+  episodes.forEach(data => {
+    console.log(`____________________
+    Action: ${data.action}
+    ${data.board}
+    
+    Score: ${data.score}
+    Reward: ${data.reward}`)
   })
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import 'babel-polyfill'
 import markov from 'markovjs'
 import * as memory from 'markovjs-immutable'
 import { egreedy, greedy } from 'markovjs/policies'
-import { random, sample } from 'lodash'
+import { random, sample, times, min, max, mean, last } from 'lodash'
+import sparkly from 'sparkly'
 
 import * as format from './format'
 import * as state from './state'
@@ -18,15 +19,28 @@ const initial = state.init({
   random,
 })
 
-const α = 0.2 // learning rate
-const γ = 0.2 // discount factor
+const α = 0.8 // learning rate
+const γ = 0.9 // discount factor
 const ε = 0.1 // exploration rate
 
-markov()
+const trainedMaschine = markov()
   .memory(memory, memory.init(0.0, a => a))
   .policies(sample, greedy) // egreedy(ε)
   .game(game, initial)
   .train(1000, α, γ)
-  .play((
-    e, // eslint-disable-next-line no-console
-  ) => format.episode(e))
+// .play((
+//   e, // eslint-disable-next-line no-console
+// ) => format.episode(e))
+const results = []
+
+times(100, () => {
+  trainedMaschine.play(e => {
+    results.push(last(format.toData(e)).score)
+  })
+})
+
+// console.log(`results: ${results}`)
+console.log(`Min: ${min(results)}`)
+console.log(`Mean: ${mean(results)}`)
+console.log(`Max: ${max(results)}`)
+console.log(sparkly(results))
